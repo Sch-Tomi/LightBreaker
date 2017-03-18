870462b3c7eaa6a8cc51d238fb70ac1429241717
SchTomi <sch.tom93@gmail.com>
2017-03-18 15:19:55 +0100

    Fixed #4

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ class Game {
 
   run(){
     // Let's play this game!
-    this.level1()
+    this.level2()
     this.main();
   }
 
@@ -24,4 +24,15 @@ class Game {
     this.canvas.addBlock(new Target(0,3,3,false,true))
     this.canvas.addParkingBlock(new DoubleMirror(0,0,0,true,true))
   }
+
+  level2(){
+    this.canvas.addBlock(new Laser(270,0,0,false,true))
+    this.canvas.addBlock(new Target(90,4,0,false,true))
+    this.canvas.addBlock(new DoubleMirror(90,3,1,false,false))
+
+    this.canvas.addParkingBlock(new Target(0,0,0,true,true))
+    this.canvas.addParkingBlock(new HalfMirror(0,0,0,true,true))
+
+  }
+
 }
