fd2596c01388812efd6008975f54e466c49225c1
SchTomi <sch.tom93@gmail.com>
2017-03-19 09:47:06 +0100

    Fixed #7

--- js/canvas.js
+++ js/canvas.js
@@ -1,242 +1,254 @@
 class Canvas {
-  constructor() {
-    this.canvas = document.createElement("canvas")
-    this.ctx = this.canvas.getContext("2d")
-    this.canvas.width = 640
-    this.canvas.height = 480
-    document.body.appendChild(this.canvas);
+    constructor() {
+        this.canvas = document.createElement("canvas")
+        this.ctx = this.canvas.getContext("2d")
+        this.canvas.width = 640
+        this.canvas.height = 480
+        document.body.appendChild(this.canvas);
 
-    this.blocks = []
-    this.moving_block_id = null
+        this.blocks = []
+        this.moving_block_id = null
 
-    this.canvas.onmousedown = (e) => {this.mouseDown(e)}
-    this.canvas.onmouseup = (e) => {this.mouseUp(e)}
-    this.canvas.ondblclick = (e) => {this.mouseClick(e)}
-
-    this._board_w = 450
-    this.cell_w = 90
-    this.cell_wh = this.cell_w / 2
-    this.table_p = 2
-
-
-    this._lock = new Image()
-    this._lock.src = "img/lock.png"
-
-    // true - free | false - reserved
-    this._parking = [true,true,true,true,true]
-
-  }
-
-  drawBoard(){
-    var bw = this._board_w
-    var bh = this._board_w
-    var p  = this.table_p
-    var cw = this.cell_w
-    var ch = this.cell_w
-
-    for (var x = 0; x <= bw; x += cw) {
-        this.ctx.moveTo(0.5 + x + p, p)
-        this.ctx.lineTo(0.5 + x + p, bh + p)
-    }
-
-
-    for (var y = 0; y <= bh; y += ch) {
-        this.ctx.moveTo(p, 0.5 + y + p)
-        this.ctx.lineTo(bw + p, 0.5 + y + p);
-    }
-
-    this.ctx.strokeStyle = "black"
-    this.ctx.stroke()
-  }
-
-  drawParking(){
-    var bw = 90
-    var bh = 450
-    var p  = 500
-    var cw = this.cell_w
-    var ch = this.cell_w
-
-    for (var x = 0; x <= bw; x += cw) {
-        this.ctx.moveTo(0.5 + x + p , 0)
-        this.ctx.lineTo(0.5 + x + p, bh )
-    }
-
-
-    for (var y = 0; y <= bh; y += ch) {
-        this.ctx.moveTo(p, 0.5 + y )
-        this.ctx.lineTo(bw + p, 0.5 + y );
-    }
-
-    this.ctx.strokeStyle = "black"
-    this.ctx.stroke()
-  }
-
-  render(){
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.drawBoard()
-    this.drawParking()
-
-    for (var block of this.blocks) {
-      this.drawRotatedImage(block.get_img(),block.get_x(),block.get_y(),block.get_angle());
-
-      if (block.locked()) {
-        this.drawLock(block)
-      }
-    }
-
-  }
-
-  addBlock(block){
-    var x = block.get_x()*this.cell_w + this.cell_wh + this.table_p
-    var y = block.get_y()*this.cell_w + this.cell_wh + this.table_p
-
-    block.canvas_repositioning(x,y)
-    this.blocks.push(block)
-  }
-
-  addParkingBlock(block){
-    this.parkingBlock(block)
-    this.blocks.push(block)
-  }
-
-  parkingBlock(block){
-    var free = this.getFreeParking()
-
-    block.set_x(545)
-    block.set_y((free*90)+45)
-    this._parking[free] = false
-  }
-
-  getFreeParking(){
-    for (var i = 0; i < this._parking.length; i++) {
-      if (this._parking[i]) {
-        return i
-      }
-    }
-  }
-
-  isInTheParking(block){
-    return block.get_x() > this._board_w
-  }
-
-  getParkingID(block){
-    return Math.round(block.get_y() / 45)-1
-  }
-
-  mouseDown(e){
-    this.searchBlock(e.clientX,e.clientY)
-
-    if(this.isInTheParking(this.blocks[this.moving_block_id])){
-      var parkingID = this.getParkingID(this.blocks[this.moving_block_id])
-      this._parking[parkingID] = true
-    }
-
-    this.canvas.onmousemove = (e) => {this.mouseMove(e)}
-  }
-
-  mouseUp(){
-    this.fitBlockToCell()
-
-    if(this.blockCollision()){
-      this.parkingBlock(this.blocks[this.moving_block_id])
-    }
-
-    this.canvas.onmousemove = null
-    this.moving_block_id = null
-  }
-
-  mouseMove(e){
-    if(this.moving_block_id !== null){
-      this.blocks[this.moving_block_id].set_x(e.clientX )
-      this.blocks[this.moving_block_id].set_y(e.clientY )
-    }
-  }
-
-  mouseClick(e){
-
-    this.searchBlock(e.clientX,e.clientY)
-
-    if(e.shiftKey){
-      this.blocks[this.moving_block_id].rotate_right()
-    }else {
-      this.blocks[this.moving_block_id].rotate_left()
-    }
-
-    this.moving_block_id = null
-
-  }
-
-  searchBlock(x,y){
-    for (var i = 0; i<this.blocks.length;i++) {
-      var block = this.blocks[i]
-      if( (block.get_x() - 45 <= x &&  x <= block.get_x() + 45) && (block.get_y() - 45 <= y &&  y <= block.get_y() + 45) ){
-        this.moving_block_id = i
-        break
-      }
-    }
-  }
-
-  fitBlockToCell(){
-    var block = this.blocks[this.moving_block_id]
-    var row = Math.round(block.get_x() / this.cell_wh)
-    var col = Math.round(block.get_y() / this.cell_wh)
-    var x = row * this.cell_wh
-    var y = col * this.cell_wh
-
-
-
-    if(x > this._board_w || y > this._board_w){
-      this.parkingBlock(block)
-    }else{
-      // between 2 cell
-      if((x - 45) % 90 > 0 || (y - 45) % 90 > 0){
-        this.parkingBlock(block)
-      }else {
-        console.log("("+x+", "+y+")");
-        block.set_x(x + this.table_p)
-        block.set_y(y + this.table_p)
-      }
-
-    }
-  }
-
-  blockCollision(){
-    for (var i = 0; i < this.blocks.length; i++) {
-      if(i != this.moving_block_id){
-        if(this.blocks[i].get_x() == this.blocks[this.moving_block_id].get_x() && this.blocks[i].get_y() == this.blocks[this.moving_block_id].get_y() ){
-          return true
+        this.canvas.onmousedown = (e) => {
+            this.mouseDown(e)
         }
-      }
+        this.canvas.onmouseup = (e) => {
+            this.mouseUp(e)
+        }
+        this.canvas.ondblclick = (e) => {
+            this.mouseClick(e)
+        }
+
+        this._board_w = 450
+        this.cell_w = 90
+        this.cell_wh = this.cell_w / 2
+        this.table_p = 2
+
+
+        this._lock = new Image()
+        this._lock.src = "img/lock.png"
+
+        // true - free | false - reserved
+        this._parking = [true, true, true, true, true]
+
     }
-    return false
-  }
+    clear() {
+        this.blocks = []
+        this._parking = [true, true, true, true, true]
+    }
+
+    drawBoard() {
+        var bw = this._board_w
+        var bh = this._board_w
+        var p = this.table_p
+        var cw = this.cell_w
+        var ch = this.cell_w
+
+        for (var x = 0; x <= bw; x += cw) {
+            this.ctx.moveTo(0.5 + x + p, p)
+            this.ctx.lineTo(0.5 + x + p, bh + p)
+        }
 
 
-  //http://creativejs.com/2012/01/day-10-drawing-rotated-images-into-canvas/
-  // ELMOSÓDIK?!
-  drawRotatedImage(image, x, y, angle){
-    var TO_RADIANS = Math.PI/180;
-  	// save the current co-ordinate system
-  	// before we screw with it
-  	this.ctx.save();
+        for (var y = 0; y <= bh; y += ch) {
+            this.ctx.moveTo(p, 0.5 + y + p)
+            this.ctx.lineTo(bw + p, 0.5 + y + p);
+        }
 
-  	// move to the middle of where we want to draw our image
-  	this.ctx.translate(x, y);
+        this.ctx.strokeStyle = "black"
+        this.ctx.stroke()
+    }
 
-  	// rotate around that point, converting our
-  	// angle from degrees to radians
-  	this.ctx.rotate(angle * TO_RADIANS);
+    drawParking() {
+        var bw = 90
+        var bh = 450
+        var p = 500
+        var cw = this.cell_w
+        var ch = this.cell_w
 
-  	// draw it up and to the left by half the width
-  	// and height of the image
-  	this.ctx.drawImage(image, -(Math.round(image.width/2)), -(Math.round(image.height/2)));
+        for (var x = 0; x <= bw; x += cw) {
+            this.ctx.moveTo(0.5 + x + p, 0)
+            this.ctx.lineTo(0.5 + x + p, bh)
+        }
 
-  	// and restore the co-ords to how they were when we began
-  	this.ctx.restore();
-  }
 
-  drawLock(block){
-    this.ctx.drawImage(this._lock, block.get_x()+20, block.get_y()-40)
-  }
+        for (var y = 0; y <= bh; y += ch) {
+            this.ctx.moveTo(p, 0.5 + y)
+            this.ctx.lineTo(bw + p, 0.5 + y);
+        }
+
+        this.ctx.strokeStyle = "black"
+        this.ctx.stroke()
+    }
+
+    render() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawBoard()
+        this.drawParking()
+
+        for (var block of this.blocks) {
+            this.drawRotatedImage(block.get_img(), block.get_x(), block.get_y(), block.get_angle());
+
+            if (block.locked()) {
+                this.drawLock(block)
+            }
+        }
+
+    }
+
+    addBlock(block) {
+        var x = block.get_x() * this.cell_w + this.cell_wh + this.table_p
+        var y = block.get_y() * this.cell_w + this.cell_wh + this.table_p
+
+        block.canvas_repositioning(x, y)
+        this.blocks.push(block)
+    }
+
+    addParkingBlock(block) {
+        this.parkingBlock(block)
+        this.blocks.push(block)
+    }
+
+    parkingBlock(block) {
+        var free = this.getFreeParking()
+
+        block.set_x(545)
+        block.set_y((free * 90) + 45)
+        this._parking[free] = false
+    }
+
+    getFreeParking() {
+        for (var i = 0; i < this._parking.length; i++) {
+            if (this._parking[i]) {
+                return i
+            }
+        }
+    }
+
+    isInTheParking(block) {
+        return block.get_x() > this._board_w
+    }
+
+    getParkingID(block) {
+        return Math.round(block.get_y() / 45) - 1
+    }
+
+    mouseDown(e) {
+        this.searchBlock(e.clientX, e.clientY)
+
+        if (this.isInTheParking(this.blocks[this.moving_block_id])) {
+            var parkingID = this.getParkingID(this.blocks[this.moving_block_id])
+            this._parking[parkingID] = true
+        }
+
+        this.canvas.onmousemove = (e) => {
+            this.mouseMove(e)
+        }
+    }
+
+    mouseUp() {
+        this.fitBlockToCell()
+
+        if (this.blockCollision()) {
+            this.parkingBlock(this.blocks[this.moving_block_id])
+        }
+
+        this.canvas.onmousemove = null
+        this.moving_block_id = null
+    }
+
+    mouseMove(e) {
+        if (this.moving_block_id !== null) {
+            this.blocks[this.moving_block_id].set_x(e.clientX)
+            this.blocks[this.moving_block_id].set_y(e.clientY)
+        }
+    }
+
+    mouseClick(e) {
+
+        this.searchBlock(e.clientX, e.clientY)
+
+        if (e.shiftKey) {
+            this.blocks[this.moving_block_id].rotate_right()
+        } else {
+            this.blocks[this.moving_block_id].rotate_left()
+        }
+
+        this.moving_block_id = null
+
+    }
+
+    searchBlock(x, y) {
+        for (var i = 0; i < this.blocks.length; i++) {
+            var block = this.blocks[i]
+            if ((block.get_x() - 45 <= x && x <= block.get_x() + 45) && (block.get_y() - 45 <= y && y <= block.get_y() + 45)) {
+                this.moving_block_id = i
+                break
+            }
+        }
+    }
+
+    fitBlockToCell() {
+        var block = this.blocks[this.moving_block_id]
+        var row = Math.round(block.get_x() / this.cell_wh)
+        var col = Math.round(block.get_y() / this.cell_wh)
+        var x = row * this.cell_wh
+        var y = col * this.cell_wh
+
+
+
+        if (x > this._board_w || y > this._board_w) {
+            this.parkingBlock(block)
+        } else {
+            // between 2 cell
+            if ((x - 45) % 90 > 0 || (y - 45) % 90 > 0) {
+                this.parkingBlock(block)
+            } else {
+                console.log("(" + x + ", " + y + ")");
+                block.set_x(x + this.table_p)
+                block.set_y(y + this.table_p)
+            }
+
+        }
+    }
+
+    blockCollision() {
+        for (var i = 0; i < this.blocks.length; i++) {
+            if (i != this.moving_block_id) {
+                if (this.blocks[i].get_x() == this.blocks[this.moving_block_id].get_x() && this.blocks[i].get_y() == this.blocks[this.moving_block_id].get_y()) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+
+    //http://creativejs.com/2012/01/day-10-drawing-rotated-images-into-canvas/
+    // ELMOSÓDIK?!
+    drawRotatedImage(image, x, y, angle) {
+        var TO_RADIANS = Math.PI / 180;
+        // save the current co-ordinate system
+        // before we screw with it
+        this.ctx.save();
+
+        // move to the middle of where we want to draw our image
+        this.ctx.translate(x, y);
+
+        // rotate around that point, converting our
+        // angle from degrees to radians
+        this.ctx.rotate(angle * TO_RADIANS);
+
+        // draw it up and to the left by half the width
+        // and height of the image
+        this.ctx.drawImage(image, -(Math.round(image.width / 2)), -(Math.round(image.height / 2)));
+
+        // and restore the co-ords to how they were when we began
+        this.ctx.restore();
+    }
+
+    drawLock(block) {
+        this.ctx.drawImage(this._lock, block.get_x() + 20, block.get_y() - 40)
+    }
 
 }
--- js/game.js
+++ js/game.js
@@ -1,50 +1,85 @@
 class Game {
-  constructor() {
-    this.canvas = new Canvas()
-    this.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
+    constructor() {
+        this.canvas = new Canvas()
+        this.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
 
-    document.body.oncontextmenu = () => {return false}
-  }
+        document.body.oncontextmenu = () => {
+            return false
+        }
 
-  main(){
-  	this.canvas.render();
 
-  	// Request to do this again ASAP
-    requestAnimationFrame(()=>{this.main()});
-  }
+        this.selector = document.createElement("select")
+        this.selector.id = "lvlSelect"
+        this.selector.innerHTML += "<option disabled selected value> -- select an level -- </option>"
+        this.selector.innerHTML += "<option value='1'>Level 1</option>"
+        this.selector.innerHTML += "<option value='2'>Level 2</option>"
+        this.selector.innerHTML += "<option value='3'>Level 3</option>"
 
-  run(){
-    // Let's play this game!
-    this.level3()
-    this.main();
-  }
+        document.body.appendChild(this.selector)
+        this.selector.onchange = () => {
+            this.levelSelect()
+        }
+    }
 
-  level1(){
-    this.canvas.addBlock(new Laser(180,1,1,false,false))
-    this.canvas.addBlock(new Target(0,3,3,false,true))
-    this.canvas.addParkingBlock(new DoubleMirror(0,0,0,true,true))
-  }
+    main() {
+        this.canvas.render();
 
-  level2(){
-    this.canvas.addBlock(new Laser(270,0,0,false,true))
-    this.canvas.addBlock(new Target(90,4,0,false,true))
-    this.canvas.addBlock(new DoubleMirror(90,3,1,false,false))
+        // Request to do this again ASAP
+        requestAnimationFrame(() => {
+            this.main()
+        });
+    }
 
-    this.canvas.addParkingBlock(new Target(0,0,0,true,true))
-    this.canvas.addParkingBlock(new HalfMirror(0,0,0,true,true))
-  }
+    run() {
+        // Let's play this game!
+        this.main();
+    }
 
-  level3(){
-    this.canvas.addBlock(new Laser(90,1,2,false,true))
-    this.canvas.addBlock(new Mirror(270,2,0,false,true))
-    this.canvas.addBlock(new Mirror(270,4,0,false,true))
-    this.canvas.addBlock(new Mirror(0,3,2,false,false))
-    this.canvas.addBlock(new DoubleMirror(90,0,4,false,false))
-    this.canvas.addBlock(new CheckPoint(0,4,3,false,false))
+    levelSelect() {
+        this.canvas.clear()
+        console.log(this.selector.options[this.selector.selectedIndex].value);
+        switch (parseInt(this.selector.options[this.selector.selectedIndex].value)) {
+            case 1:
+                this.level1()
+                console.log('asd');
+                break;
+            case 2:
+                this.level2()
+                break;
+            case 3:
+                this.level3()
+                break;
+            default:
 
-    this.canvas.addParkingBlock(new Mirror(0,0,0,true,true))
-    this.canvas.addParkingBlock(new Mirror(0,0,0,true,true))
-    this.canvas.addParkingBlock(new HalfMirror(0,0,0,true,true))
-  }
+        }
+    }
+
+    level1() {
+        this.canvas.addBlock(new Laser(180, 1, 1, false, false))
+        this.canvas.addBlock(new Target(0, 3, 3, false, true))
+        this.canvas.addParkingBlock(new DoubleMirror(0, 0, 0, true, true))
+    }
+
+    level2() {
+        this.canvas.addBlock(new Laser(270, 0, 0, false, true))
+        this.canvas.addBlock(new Target(90, 4, 0, false, true))
+        this.canvas.addBlock(new DoubleMirror(90, 3, 1, false, false))
+
+        this.canvas.addParkingBlock(new Target(0, 0, 0, true, true))
+        this.canvas.addParkingBlock(new HalfMirror(0, 0, 0, true, true))
+    }
+
+    level3() {
+        this.canvas.addBlock(new Laser(90, 1, 2, false, true))
+        this.canvas.addBlock(new Mirror(270, 2, 0, false, true))
+        this.canvas.addBlock(new Mirror(270, 4, 0, false, true))
+        this.canvas.addBlock(new Mirror(0, 3, 2, false, false))
+        this.canvas.addBlock(new DoubleMirror(90, 0, 4, false, false))
+        this.canvas.addBlock(new CheckPoint(0, 4, 3, false, false))
+
+        this.canvas.addParkingBlock(new Mirror(0, 0, 0, true, true))
+        this.canvas.addParkingBlock(new Mirror(0, 0, 0, true, true))
+        this.canvas.addParkingBlock(new HalfMirror(0, 0, 0, true, true))
+    }
 
 }
